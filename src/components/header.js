9416cbd1a1a1e6a83f6d71097ebad8a2555a3e30
Volpe6 <baasch2014@gmail.com>
2019-10-08 22:32:42 -0300

    ajuste na pesquisa

--- src/components/header.js
+++ src/components/header.js
@@ -20,17 +20,20 @@ export default class Header extends Component {
     trocaClasse = () => {
         let divDestacada = document.getElementById("div-caixa-oculta");
         let divOverlay   = document.getElementById("header-overlay");
+        let btnIcon      = document.getElementById("btn-filtro");
 
         if(divDestacada.classList.contains('oculta')) {
             divDestacada.classList.remove('oculta');
             divDestacada.classList.add('destaca');
             divOverlay.classList.remove('opacidade-oculta');
             divOverlay.classList.add('opacidade-visivel');
+            btnIcon.classList.add('destaca');
         } else {
             divDestacada.classList.add('oculta');
             divDestacada.classList.remove('destaca');
             divOverlay.classList.add('opacidade-oculta');
             divOverlay.classList.remove('opacidade-visivel');
+            btnIcon.classList.remove('destaca');
         }
     };
 
@@ -49,11 +52,23 @@ export default class Header extends Component {
 
         oFiltro.conteudo = document.getElementsByName('input-busca')[0].value;
 
+        document.getElementsByName('input-busca')[0].value = '';
+        
         this.setState({
             redireciona : true,
             filtro      : JSON.stringify(oFiltro)
         });
         
+    };
+
+    teclaPressionada = function(e) {
+        switch (e.keyCode) {
+            case 13:
+                this.busca();
+                break;
+            default:
+                break;
+        }
     };
 
     redireciona = () => {
@@ -84,10 +99,11 @@ export default class Header extends Component {
                                 type="text" 
                                 placeholder="Busca"
                                 name="input-busca"
+                                onKeyDown={e => this.teclaPressionada(e)}
                             />
                             {this.redireciona()}
                             <button onClick={this.busca} className="btn-input"><MdSearch /></button>
-                            <button onClick={this.trocaClasse} className="btn-ic-clicavel"><MdFilterList /></button>
+                            <button onClick={this.trocaClasse} id="btn-filtro" className="btn-ic-clicavel"><MdFilterList /></button>
                         </div>
                     </div>
                     <div id="div-caixa-oculta" className="caixa-oculta oculta">
